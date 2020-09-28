c5d7bd4808732e38fde154fdaa03a3609b3b2cb8
sulojana1107 <sulojana1107@gmail.com>
2020-09-28 10:41:55 +0530

    Document.java modified with get and set methods

---
+++ forstash/Document.java
@@ -0,0 +1,27 @@
+class Document
+{
+	private String name;
+	private String extension;
+	private String content;
+	private int id;
+
+	public void setName();
+	{
+	}
+	public void setContent();
+	{
+	}
+	public void setExtension();
+	{
+	}
+	public String getName();
+	{
+	}
+	public String getContent();
+	{
+	}
+	public String getExtension();
+	{
+	}
+
+}
